ac96f819d5c9b6bf9ffe70125d01a967012050fe
Goosebumps <tkadzzz@gmail.com>
2022-03-30 00:33:18 +0200

    Attachme Commit

--- student/includes/navbar.inc.php
+++ student/includes/navbar.inc.php
@@ -140,6 +140,7 @@ if (isset($_GET['logout']) && ($_GET['logout'] == 'true')) {
                         <p class="mb-1 mt-3 font-weight-semibold">Allen Moreno</p>
                         <p class="fw-light text-muted mb-0">allenmoreno@gmail.com</p>
                     </div>
+                    
                     <a class="dropdown-item" href="profile.php"><i class="dropdown-item-icon mdi mdi-account-outline text-primary me-2"></i> My Profile <span class="badge badge-pill badge-danger">1</span></a>
                     <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-message-text-outline text-primary me-2"></i> Messages</a>
                     <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-calendar-check-outline text-primary me-2"></i> Activity</a>
